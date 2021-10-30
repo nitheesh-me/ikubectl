8459fcdb9a01ef63593c39d54a556ee8dbd50832
Nitheesh Chandra <nitheesh.my@gmail.com>
2021-10-30 16:58:50 +0530

    Basic Poetry setup
    - __version__ fetch from pyproject.toml
    - a testcase for the same
    
    NOTE: no actual functionality

---
+++ ikubectl/__init__.py
@@ -0,0 +1,12 @@
+import os
+import toml
+
+
+def get_version():
+    """Function to get the version of the ikubectl package."""
+    pyproject_path = os.path.join(os.path.dirname(__file__), '../pyproject.toml')
+    with open(pyproject_path) as f:
+        pyproject = toml.load(f)
+    return pyproject['tool']['poetry']['version']
+
+__version__ = get_version()
